543d16d6b8f259f0058a7f64a4766adf0767865b
Antonio Vanegas <hpsaturn@gmail.com>
2016-11-10 23:28:55 -0500

    migrate MALOS ports to MalosTarget class

--- android/gpio_demo/app/src/main/java/admobilize/matrix/malosclient/Config.java
+++ android/gpio_demo/app/src/main/java/admobilize/matrix/malosclient/Config.java
@@ -4,10 +4,10 @@ package admobilize.matrix.malosclient;
  * Created by Antonio Vanegas @hpsaturn on 11/4/16.
  */
 public class Config {
-    public static final String CREATORIP="192.168.3.128";
 
-    public static final int    MALOS_GPIO_BASE_PORT = 20049;
-    public static final String MALOS_GPIO_CONFIG = "tcp://"+CREATORIP+":"+MALOS_GPIO_BASE_PORT;
-    public static final String MALOS_GPIO_SUB = "tcp://"+CREATORIP+":"+(MALOS_GPIO_BASE_PORT+3);
+    public static final boolean DEBUG = true;
+
+    public static final String CREATORIP="192.168.1.114";
+
 
 }
