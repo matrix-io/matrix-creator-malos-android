5bf345a4653b70ac47075d4e7c3e52b65289001a
Antonio Vanegas <hpsaturn@gmail.com>
2016-11-11 21:45:40 -0500

    change test ip

--- android/gpio_demo/app/src/main/java/admobilize/matrix/malosclient/Config.java
+++ android/gpio_demo/app/src/main/java/admobilize/matrix/malosclient/Config.java
@@ -7,7 +7,7 @@ public class Config {
 
     public static final boolean DEBUG = true;
 
-    public static final String CREATORIP="192.168.3.128";
+    public static final String CREATORIP="192.168.1.114";
 
 
 }
