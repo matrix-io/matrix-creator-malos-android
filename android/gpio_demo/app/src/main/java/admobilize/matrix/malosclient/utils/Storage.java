1e3b34bd064a7c7c5f69e3243686ba733046c307
Antonio Vanegas <hpsaturn@gmail.com>
2016-11-13 00:36:08 -0500

    add new storage library and UI utils

---
+++ android/gpio_demo/app/src/main/java/admobilize/matrix/malosclient/utils/Storage.java
@@ -0,0 +1,12 @@
+package admobilize.matrix.malosclient.utils;
+
+/**
+ * Created by Antonio Vanegas @hpsaturn on 11/12/16.
+ */
+
+public class Storage {
+
+    public static final String CURRENT_DEVICE = "KEY_CURRENT_DEVICE" ;
+
+
+}
