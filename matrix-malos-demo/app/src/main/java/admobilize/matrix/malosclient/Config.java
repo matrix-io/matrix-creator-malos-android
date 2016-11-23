92aff706cfa645773061cc08394e324ba6db3410
Antonio Vanegas <hpsaturn@gmail.com>
2016-11-23 09:37:35 -0500

    add pin GPIO output/input constant config

--- matrix-malos-demo/app/src/main/java/admobilize/matrix/malosclient/Config.java
+++ matrix-malos-demo/app/src/main/java/admobilize/matrix/malosclient/Config.java
@@ -6,9 +6,11 @@ package admobilize.matrix.malosclient;
 public class Config {
 
     public static final boolean DEBUG = true;
-    public static final boolean VERBOSE = DEBUG&&true;
+    public static final boolean VERBOSE = DEBUG&&false;
 
     public static final int LED_INTENSITY_DIVISOR = 10;
     public static final long TIME_INTO_PING = 7000;
 
+    public static final short GPIO_DEMO_OUTPUT = 0;
+    public static final short GPIO_DEMO_INPUT  = 1;
 }
