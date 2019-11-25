cf3b1ed9abe5ff2b858dc076c08d61f8d2db56fd
Antonio Vanegas <hpsaturn@gmail.com>
2019-11-25 13:09:00 +0100

    testing fake fabric keys

--- matrix-malos-demo/cibuild.sh
+++ matrix-malos-demo/cibuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 #fake fabrik keys
-echo "apiSecret=3da2xx"  > app/fabric.properties
-echo "apiKey=48be76xxx" >> app/fabric.properties
+
+echo "apiSecret=3da2e6ea00f6074d080bdda7c11d6df97c00b8b01cdcb3ef765b7be100e8ebf0"  > app/fabric.properties
+echo "apiKey=48be76c412597fd97e8cdd86ec60135e9d46f5a2" >> app/fabric.properties
 
 ./gradlew assembleDebug
